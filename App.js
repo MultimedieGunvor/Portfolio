58fc3a319790638f51230954fc5b42b0126e2361
MultimedieGunvor <90311518+MultimedieGunvor@users.noreply.github.com>
2022-09-26 09:49:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import Projects from "./pages/Projects";
 import Nav from "./components/Nav";
 import { LangProvider } from './Contexts/LangContext';
+import AboutPage from "./pages/AboutPage";
 
 function App(props) {
     
@@ -15,6 +16,7 @@ function App(props) {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/projects" element={<Projects />} />
+                    <Route path="/about" element={<AboutPage />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </LangProvider>
